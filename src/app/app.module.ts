0d97b726d934ba8e473506df1d358a890a9c3a15
Damian Jorda <djorda@quadiontech.com>
2020-06-07 15:15:56 -0300

    arreglado effectos registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './componentes/error/error.component';
 import { LoginComponent } from './componentes/login/login.component';
@@ -8,6 +9,9 @@ import { RegistroComponent } from './componentes/registro/registro.component';
 import { AngularFirebaseModule } from './modulos/angular-firebase/angular-firebase.module';
 import { AngularMaterialModule } from './modulos/angular-material/angular-material.module';
 import { RuteoModule } from './modulos/ruteo/ruteo.module';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
 
 
 @NgModule({
@@ -19,10 +23,13 @@ import { RuteoModule } from './modulos/ruteo/ruteo.module';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FormsModule,
     RuteoModule,
+    MatNativeDateModule,
     AngularMaterialModule,
-    AngularFirebaseModule
+    AngularFirebaseModule,
+    ReactiveFormsModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
