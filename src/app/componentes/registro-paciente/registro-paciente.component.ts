fc77502fa915a53b18955c3f1a5ab62e9f783f1c
Damian Jorda <djorda@quadiontech.com>
2020-06-10 19:29:13 -0300

    estilos para paciente imagen

--- src/app/componentes/registro-paciente/registro-paciente.component.ts
+++ src/app/componentes/registro-paciente/registro-paciente.component.ts
@@ -1,92 +1,27 @@
-import { transition, trigger, style, animate } from '@angular/animations';
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { Router } from '@angular/router';
-import { TipoUsuario } from 'src/app/enum/tipo-usuario.enum';
-import { AuthService } from 'src/app/servicios/auth.service';
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-	isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-		const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
-		const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
-
-		return (invalidCtrl || invalidParent);
-	}
-}
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-registro-paciente',
-  templateUrl: './registro-paciente.component.html',
-  styleUrls: ['./registro-paciente.component.css']
+	selector: 'app-registro-paciente',
+	templateUrl: './registro-paciente.component.html',
+	styleUrls: ['./registro-paciente.component.css']
 })
 export class RegistroPacienteComponent implements OnInit {
-
-  signUpForm = new FormGroup({
-		usuario: new FormControl('', Validators.required),
-		clave: new FormControl('', Validators.required),
-		confirmarClave: new FormControl('', Validators.required),
-		email: new FormControl('', Validators.required),
-		tipo: new FormControl('', Validators.required)
-	}, this.validarClaves);
-
-	hideClave = true;
-	hideConfirmarClave = true;
-	tiposUsuario = TipoUsuario;
-	matcher = new MyErrorStateMatcher();
-	tipoUsuario: TipoUsuario;
-
-	esRegistroAdmin = false;
-	esRegistroPaciente = false;
-	esRegistroProfesional = false;
-
-	constructor(
-		private authService: AuthService,
-		private router: Router) {
+	url = '';
+	constructor() {
 	}
 
 	ngOnInit() {
 	}
 
-	validarClaves(group: FormGroup) {
-		const clave = group.controls.clave.value;
-		const confirmarClave = group.controls.confirmarClave.value;
-		return clave === confirmarClave ? null : { notSame: true };
-	}
+	onSelectFile(event) {
+		if (event.target.files && event.target.files[0]) {
+			var reader = new FileReader();
 
-	cancelar() {
-		this.router.navigate(['']);
-	}
+			reader.readAsDataURL(event.target.files[0]); // read file as data url
 
-	registrarUsuario() {
-		const usuario = this.signUpForm.controls.usuario.value;
-		const clave = this.signUpForm.controls.clave.value;
-		this.authService.registrarUsuario(usuario, clave);
-		this.router.navigate(['']);
-	}
-
-	onSelectionChange = ({ value }) => {
-		switch (<any>TipoUsuario[value]) {
-			case this.tiposUsuario.ADMIN:
-				this.esRegistroPaciente = false;
-				this.esRegistroProfesional = false;
-				this.esRegistroAdmin = true;
-				break;
-			case this.tiposUsuario.PACIENTE:
-				this.esRegistroPaciente = true;
-				this.esRegistroProfesional = false;
-				this.esRegistroAdmin = false;
-				break;
-			case this.tiposUsuario.PROFESIONAL:
-				this.esRegistroPaciente = false;
-				this.esRegistroProfesional = true;
-				this.esRegistroAdmin = false;
-				break;
-			default:
-				this.esRegistroPaciente = false;
-				this.esRegistroProfesional = false;
-				this.esRegistroAdmin = false;
-				break;
+			reader.onload = (event) => { // called once readAsDataURL is completed
+				this.url = event.target.result.toString();
+			}
 		}
 	}
 }
