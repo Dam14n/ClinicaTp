cee5ef4e570a81fdad306f182a3a707ea13948c0
Damian Jorda <djorda@quadiontech.com>
2020-06-11 21:56:49 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { AppComponent } from './app.component';
 import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { CargarTurnosComponent } from './componentes/cargar-turnos/cargar-turnos.component';
 import { ErrorComponent } from './componentes/error/error.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { MenuComponent } from './componentes/menu/menu.component';
@@ -13,17 +16,14 @@ import { RegistroMasMenuComponent } from './componentes/registro-mas-menu/regist
 import { RegistroPacienteComponent } from './componentes/registro-paciente/registro-paciente.component';
 import { RegistroProfesionalComponent } from './componentes/registro-profesional/registro-profesional.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
+import { VerTurnosComponent } from './componentes/ver-turnos/ver-turnos.component';
+import { VerUsuariosComponent } from './componentes/ver-usuarios/ver-usuarios.component';
 import { AngularFirebaseModule } from './modulos/angular-firebase/angular-firebase.module';
 import { AngularMaterialModule } from './modulos/angular-material/angular-material.module';
 import { RuteoModule } from './modulos/ruteo/ruteo.module';
 import { EnumToArrayPipe } from './pipe/enum-to-array.pipe';
 import { ExcludeFilterPipe } from './pipe/exclude-filter.pipe';
-import { VerTurnosComponent } from './componentes/ver-turnos/ver-turnos.component';
-import { CargarTurnosComponent } from './componentes/cargar-turnos/cargar-turnos.component';
-import { VerUsuariosComponent } from './componentes/ver-usuarios/ver-usuarios.component';
-
-
-
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -51,7 +51,12 @@ import { VerUsuariosComponent } from './componentes/ver-usuarios/ver-usuarios.co
     MatNativeDateModule,
     AngularMaterialModule,
     AngularFirebaseModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbModalModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   providers: [EnumToArrayPipe, ExcludeFilterPipe],
   bootstrap: [AppComponent]
